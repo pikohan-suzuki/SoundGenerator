ccc375fb70547e8a8557fc79e47b56fa0ecebd37
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-11 21:54:28 +0900

    add navigation relation

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/MainActivity.kt
@@ -2,11 +2,17 @@ package com.amebaownd.pikohan_nwiatori.soundgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupWithNavController(bottomNavigationView,navController)
     }
 }
