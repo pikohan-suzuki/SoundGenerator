d93e4c2d203b66ab2579eb595cceddd70fd6d1ad
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-12 23:37:02 +0900

    MetronomeFragment

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/metronome/MetronomeFragment.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/metronome/MetronomeFragment.kt
@@ -5,15 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.amebaownd.pikohan_nwiatori.soundgenerator.R
+import com.amebaownd.pikohan_nwiatori.soundgenerator.databinding.FragmentMetronomeBinding
+import com.amebaownd.pikohan_nwiatori.soundgenerator.util.getViewModelFactory
 
 class MetronomeFragment :Fragment(){
+
+    private val viewModel:MetronomeViewModel by viewModels{getViewModelFactory()}
+
+    private lateinit var fragmentMetronomeBinding: FragmentMetronomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_metronome,container,false)
+        ): View? {
+        fragmentMetronomeBinding = FragmentMetronomeBinding.inflate(inflater,container,false).apply {
+            viewModel = this@MetronomeFragment.viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+        return fragmentMetronomeBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
     }
 }
