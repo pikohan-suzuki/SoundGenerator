5b37c8ac9a25146f4940d42a6ad4fa3c6eb5cb2f
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-15 22:07:39 +0900

    add check permission

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.amebaownd.pikohan_nwiatori.soundgenerator
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.amebaownd.pikohan_nwiatori.soundgenerator.util.MyContext
@@ -9,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val RECORD_AUDIO_REQUEST_CODE =101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,5 +22,13 @@ class MainActivity : AppCompatActivity() {
         setupWithNavController(bottomNavigationView,navController)
 
         MyContext.onCreateApplication(applicationContext)
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.RECORD_AUDIO)){
+
+            }else{
+                ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.RECORD_AUDIO),RECORD_AUDIO_REQUEST_CODE)
+            }
+        }
     }
 }
