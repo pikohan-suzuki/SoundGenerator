b1d04bf8de343d8cb49f2d7149d4519172d90479
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-15 22:32:32 +0900

    add reset method

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/tuner/TunerFragment.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/soundgenerator/tuner/TunerFragment.kt
@@ -1,27 +1,34 @@
 package com.amebaownd.pikohan_nwiatori.soundgenerator.tuner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.amebaownd.pikohan_nwiatori.soundgenerator.R
 import com.amebaownd.pikohan_nwiatori.soundgenerator.databinding.FragmentTunerBinding
 import com.amebaownd.pikohan_nwiatori.soundgenerator.util.getViewModelFactory
+import android.view.Gravity
 
-class TunerFragment :Fragment(){
 
-    private val viewModel :TunerViewModel by viewModels{getViewModelFactory()}
 
-    private lateinit var fragmentTunerBinding:FragmentTunerBinding
+class TunerFragment : Fragment() {
+
+    private val viewModel: TunerViewModel by viewModels { getViewModelFactory() }
+
+    private lateinit var fragmentTunerBinding: FragmentTunerBinding
+    private var isPermissionGranted = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentTunerBinding = FragmentTunerBinding.inflate(inflater,container,false).apply {
+        fragmentTunerBinding = FragmentTunerBinding.inflate(inflater, container, false).apply {
             viewModel = this@TunerFragment.viewModel
             lifecycleOwner = viewLifecycleOwner
         }
@@ -31,11 +38,39 @@ class TunerFragment :Fragment(){
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        viewModel.start()
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (ContextCompat.checkSelfPermission(
+                this.context!!,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val toast = Toast.makeText(
+                this.context,
+                "Please enable microphone permissions to use this feature.", Toast.LENGTH_LONG
+            )
+            toast.setGravity(Gravity.CENTER_VERTICAL or Gravity.CENTER_HORIZONTAL, 0, 0)
+            toast.show()
+            isPermissionGranted=false
+        } else {
+            viewModel.start()
+            isPermissionGranted=true
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(isPermissionGranted)
+            viewModel.onPause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        viewModel.onStop()
+        if(isPermissionGranted)
+            viewModel.onStop()
     }
 }
